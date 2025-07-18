a2fb76151c1c59edae7a027af094977620296bd6
Vera <135285088+Vera-nur@users.noreply.github.com>
2025-07-18 13:09:31 +0300

    Add Poppins custom font support

---
+++ moviesList/moviesList/Features/Font/Poppins.swift
@@ -0,0 +1,18 @@
+//
+//  Poppins.swift
+//  moviesList
+//
+//  Created by Vera Nur on 18.07.2025.
+//
+
+import SwiftUI
+
+struct Poppins: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Poppins()
+}
